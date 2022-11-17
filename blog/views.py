f42ab7a0d371f7b956749f18e7ac4f2f00feb92d
Nusrat Ismoilov <nusrat.bbismoilov@gmail.com>
2022-11-17 21:24:15 +0900

    Update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -65,14 +65,21 @@ def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts = posts.sort(key=get_date())
-    latest_post = sorted_posts[-3:]
-    return render(request, "blog/index.html", {})
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post":  identified_post
+    })
 
          
