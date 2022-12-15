c0e824ee23ed5d3ce59338f6ce2720c29cc2735b
Nusrat Ismoilov <nusrat.bbismoilov@gmail.com>
2022-12-15 22:12:16 +0900

    Update

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
